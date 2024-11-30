9037564b979eac8d44ad20fdf4d3828e65fbb77e
1o3d <126003133+1o3d@users.noreply.github.com>
2024-11-29 18:38:07 -0700

    sd

--- testapp/views.py
+++ testapp/views.py
@@ -46,4 +46,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
     return render(request,'signup.html', {'form':form})
