dc225571889b0fbf8ce1193c6c25ec9cf2004d2b
1o3d <126003133+1o3d@users.noreply.github.com>
2024-11-29 18:20:08 -0700

    updated login

--- testapp/views.py
+++ testapp/views.py
@@ -20,8 +20,10 @@ def login(request):
 
             try:
                  # https://docs.djangoproject.com/en/5.1/topics/db/queries/
-                cust_user = Customer.objects.get(username = username)
-            except Customer.DoesNotExist:
+                 cust_user = Customer.objects.get(username = username)
+                 dist_user = Distributer.objects.get(username = username)
+                 resp_user = HealthCareRepresentative.objects.get(username = username)
+            except Customer.DoesNotExist or Distributer.DoesNotExist or HealthCareRepresentative.DoesNotExist:
                 print("Invalid username")
                 return redirect('login')
     else:
