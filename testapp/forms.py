60935ee87e603a23895a3917b6d37a4e626cbaf5
1o3d <126003133+1o3d@users.noreply.github.com>
2024-12-09 02:25:36 -0700

    fixed login to actually check the password as well

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, PasswordInput
 from .models import  Customer, Medication, CustomerPhone, CustomerEmail, InsurancePlan, MedicationIngredients, Ingredient
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100, required=True)
-    password = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=PasswordInput(),max_length=100, required=True)
 
 class SignupForm(ModelForm):
     class Meta:
